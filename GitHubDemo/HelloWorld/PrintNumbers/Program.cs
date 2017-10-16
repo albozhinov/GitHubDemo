3cf067a93585c23d137eab79a75be85e2dbf11d6
Aleksandar Bozhinov <a.bozhinov90@gmail.com>
2017-10-16 18:38:30 +0300

    Added some todo information

---
+++ GitHubDemo/HelloWorld/PrintNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline(1);
+            Console.Writeline(11);
+            Console.Writeline(1001);
+        }
+    }
+}
